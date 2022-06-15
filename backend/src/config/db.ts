66dbc86ca1aa165362d8a127bcf41c00a3a3d281
Manavendra Sen <manavendra4288@gmail.com>
2022-06-16 00:41:38 +0530

    ci: configure production docker

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,9 +1,30 @@
 import { MikroORM } from "@mikro-orm/core";
-import microConfig from "./micro-orm.config";
+import type { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import path from "path";
 
 const connect_db = async () => {
-  const orm = await MikroORM.init(microConfig);
-  await orm.getMigrator().up();
+  try {
+    const orm = await MikroORM.init<PostgreSqlDriver>({
+      migrations: {
+        path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+        glob: '!(*.d).{js,ts}', // how to match migration files (all .js and .ts files, but not .d.ts)
+      },
+      type: "postgresql",
+      port: 5432,
+      host: process.env.POSTGRES_HOST!,
+      user: process.env.POSTGRES_USER!,
+      password: process.env.POSTGRES_PASSWORD!,
+      dbName: process.env.POSTGRES_DB!,
+      debug: process.env.NODE_ENV === "development",
+      entities: ['./dist/entities/**/*.js'], // path to our JS entities (dist), relative to `baseDir`
+      entitiesTs: ['./src/entities/**/*.ts'], // path to our TS entities (source), relative to `baseDir`
+    });
+    await orm.getMigrator().up();
+    console.log(`Connected to database ${process.env.POSTGRES_HOST}:${process.env.POSTGRES_DB}`);
+  }
+  catch (error) {
+    console.log(`Could not connect to database ${process.env.POSTGRES_HOST}:${process.env.POSTGRES_DB}`, error)
+  }
 };
 
 export default connect_db;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,26 @@
 import dotenv from "dotenv";
+import express from "express";
 import path from "path";
 import config from "./config/config";
+import connect_db from "./config/db";
 
-dotenv.config({ path: path.join(__dirname, "config", "config.env") });
+dotenv.config({ path: path.join(__dirname, "../.env") });
 config(process.env.NODE_ENV || "development");
 
+const main = async () => {
+	await connect_db();
+
+	const app = express();
+
+	app.get("/", (_, res) => {
+		res.send({
+			status: "connected"
+		})
+	})
+
+	app.listen(process.env.PORT || 5000, () => {
+		console.log(`Server listening in ${process.env.NODE_ENV} on ${process.env.PORT}`)
+	})
+}
+
+main();
